4735daba7163839b545163302493f6088683fec0
Wilson Xing <wilson0398@gmail.com>
2019-04-20 23:29:15 -0700

    added a getElement by index and getSize method

--- cse360ToDoList/src/cse360ToDoList/TDList.java
+++ cse360ToDoList/src/cse360ToDoList/TDList.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class TDList {
 	
 	public ArrayList<TDElement> toDoList;
+	public int currentSortMethod = 0;													//currentSortMethod = 0 for sortByPriority, csm = 1 for sortbydescription, csm = 2 for sortbydate
 	
 	public TDList(){
 		toDoList = new ArrayList<TDElement>();
@@ -22,10 +23,20 @@ public class TDList {
 		}
 		else{
 			toDoList.add(element);
+			sortByCurrentMethod();
 			return 0;
 		}
 		
 	}
+	
+	public TDElement getElement(int index){
+		return toDoList.get(index);
+	}
+	
+	public int getListSize(){
+		return toDoList.size();
+	}
+	
 	
 	public int checkDuplicate(TDElement element){													//return 0 = no duplicate, 1 = duplicate description, 2 = duplicate priority
 		for(int el = 0; el < toDoList.size(); el++){
@@ -49,24 +60,36 @@ public class TDList {
 	}
 	
 	
+	private void sortByCurrentMethod(){
+		if(currentSortMethod == 0){
+			sortByPriority();
+		}
+		else if(currentSortMethod == 1){
+			sortByDescription();
+		}
+		else{
+			sortByDueDate();
+		}
+	}
+	
 	public void sortByDescription(){
 		Collections.sort(toDoList, new DescriptionComparator());
+		currentSortMethod = 1;
 	}
 	
 	public void sortByPriority(){
 		Collections.sort(toDoList, new PriorityComparator());
+		currentSortMethod = 0;
 	}
 	
 	public void sortByDueDate(){
 		Collections.sort(toDoList, new DateComparator());
+		currentSortMethod = 2;
 	}
 	
 	
-	
-	
-	
-	
 }
+
 
 class DescriptionComparator implements Comparator<TDElement> {
 	@Override
