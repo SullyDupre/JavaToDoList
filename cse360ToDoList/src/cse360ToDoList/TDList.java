b5b254c437ff924dd76c34eb48170079c230245b
Wilson Xing <wilson0398@gmail.com>
2019-04-20 23:12:26 -0700

    Added sorting methods to the list and a print list for debugging

--- cse360ToDoList/src/cse360ToDoList/TDList.java
+++ cse360ToDoList/src/cse360ToDoList/TDList.java
@@ -27,7 +27,6 @@ public class TDList {
 		
 	}
 	
-	
 	public int checkDuplicate(TDElement element){													//return 0 = no duplicate, 1 = duplicate description, 2 = duplicate priority
 		for(int el = 0; el < toDoList.size(); el++){
 			TDElement currentCheck = toDoList.get(el);
@@ -41,4 +40,61 @@ public class TDList {
 		return 0;
 	}
 	
+	public void printList(){
+		for(int el = 0; el < toDoList.size(); el++){
+			TDElement current = toDoList.get(el);
+			System.out.print("Description: " + current.getDescription() + "  " + "Priority: " + current.getPriority() + "   " + "Due Date: " + current.getDueDate() + "   " + "Status   " + current.getStatus());
+			System.out.println();
+		}
+	}
+	
+	
+	public void sortByDescription(){
+		Collections.sort(toDoList, new DescriptionComparator());
+	}
+	
+	public void sortByPriority(){
+		Collections.sort(toDoList, new PriorityComparator());
+	}
+	
+	public void sortByDueDate(){
+		Collections.sort(toDoList, new DateComparator());
+	}
+	
+	
+	
+	
+	
+	
 }
+
+class DescriptionComparator implements Comparator<TDElement> {
+	@Override
+	public int compare(TDElement first, TDElement second){
+		return first.getDescription().compareToIgnoreCase(second.getDescription());
+	}
+}
+
+class PriorityComparator implements Comparator<TDElement> {
+	@Override
+	public int compare(TDElement first, TDElement second){
+		if(first.getPriority() < second.getPriority()){
+			return -1;
+		}
+		else if(first.getPriority() == second.getPriority()){
+			return 0;
+		}
+		else{
+			return 1;
+		}
+	}
+}
+
+class DateComparator implements Comparator<TDElement> {
+	@Override
+	public int compare(TDElement first, TDElement second){
+		return second.getDueDate().compareTo(first.getDueDate());
+	}
+}
+
+
