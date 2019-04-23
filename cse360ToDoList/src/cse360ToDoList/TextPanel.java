10cb183e4a71a630130fa53e4a7a6fb6c464aa5f
tnguy107 <tnguy107@asu.edu>
2019-04-22 23:09:53 -0700

    edit

--- cse360ToDoList/src/cse360ToDoList/TextPanel.java
+++ cse360ToDoList/src/cse360ToDoList/TextPanel.java
@@ -71,7 +71,7 @@ public class TextPanel extends JPanel {
 			if (clicked == addButton)
 			{
 				//labels
-				JLabel addTitle = new JLabel("Add");
+				JLabel addTitle = new JLabel("ADD");
 				addTitle.setFont(new Font("Time New Roman", Font.BOLD, 20));
 				JLabel descriptionLabel = new JLabel("Description");
 				JLabel dueDateLabel = new JLabel("Due Date");
@@ -243,30 +243,214 @@ public class TextPanel extends JPanel {
 				}
 				
 			}
+			
+			
+			
 			//when edit button is click
 			if (clicked == editButton)
 			{
+				if(!jlist.isSelectionEmpty()) {
+					TDElement elem = list.getElement(jlist.getSelectedIndex());
+					//labels
+					JLabel addTitle = new JLabel("EDIT");
+					addTitle.setFont(new Font("Time New Roman", Font.BOLD, 20));
+					JLabel descriptionLabel = new JLabel("Description");
+					JLabel dueDateLabel = new JLabel("Due Date");
+					JLabel priorityLabel = new JLabel("Priority(number)");
+					JLabel statusLabel = new JLabel("Status");
+					JLabel dateStartedLabel = new JLabel("Date Started");
+					JLabel dateFinishedLabel = new JLabel("Date Finished");
+					
+					//text fields with 20 characters in each each
+					JTextField descriptionField = new JTextField(20);
+					descriptionField.setText(elem.getDescription());
+					JTextField priorityField = new JTextField(20);
+					priorityField.setText(Integer.toString(elem.getPriority()));
+					JTextField statusField = new JTextField(20);
+					JTextField dateStartedField = new JTextField(20);
+					JTextField dateFinishedField = new JTextField(20);
+					
+					//left panel
+					JPanel leftPanel = new JPanel();
+					leftPanel.setLayout(new GridLayout(6,1));
+					leftPanel.add(descriptionLabel);
+					leftPanel.add(dueDateLabel);
+					leftPanel.add(priorityLabel);
+					leftPanel.add(statusLabel);
+					leftPanel.add(dateStartedLabel);
+					leftPanel.add(dateFinishedLabel);
+					
+					//right panel
+					JPanel rightPanel = new JPanel();
+					rightPanel.setLayout(new GridLayout(6,1));
+					rightPanel.add(descriptionField);
+	
+					//drop down menu for the months
+					jmonthduedate = new JComboBox();
+					DefaultComboBoxModel jmonthdue = new DefaultComboBoxModel();
+					jmonthdue.addElement("January");
+					jmonthdue.addElement("February");
+					jmonthdue.addElement("March");
+					jmonthdue.addElement("April");
+					jmonthdue.addElement("May");
+					jmonthdue.addElement("June");
+					jmonthdue.addElement("July");
+					jmonthdue.addElement("August");
+					jmonthdue.addElement("September");
+					jmonthdue.addElement("October");
+					jmonthdue.addElement("November");
+					jmonthdue.addElement("December");
+					jmonthduedate.setModel(jmonthdue);
+					
+					
+					DefaultComboBoxModel jmonthstart = new DefaultComboBoxModel();
+					jmonthstart.addElement("January");
+					jmonthstart.addElement("February");
+					jmonthstart.addElement("March");
+					jmonthstart.addElement("April");
+					jmonthstart.addElement("May");
+					jmonthstart.addElement("June");
+					jmonthstart.addElement("July");
+					jmonthstart.addElement("August");
+					jmonthstart.addElement("September");
+					jmonthstart.addElement("October");
+					jmonthstart.addElement("November");
+					jmonthstart.addElement("December");
+					
+					DefaultComboBoxModel jmonthend = new DefaultComboBoxModel();
+					jmonthend.addElement("January");
+					jmonthend.addElement("February");
+					jmonthend.addElement("March");
+					jmonthend.addElement("April");
+					jmonthend.addElement("May");
+					jmonthend.addElement("June");
+					jmonthend.addElement("July");
+					jmonthend.addElement("August");
+					jmonthend.addElement("September");
+					jmonthend.addElement("October");
+					jmonthend.addElement("November");
+					jmonthend.addElement("December");
+					
+					//drop down menu for status bar
+					jstat = new JComboBox();
+					DefaultComboBoxModel jstatusOpt = new DefaultComboBoxModel();
+					jstatusOpt.addElement("Not Started");
+					jstatusOpt.addElement("In Progress");
+					jstatusOpt.addElement("Finished");
+					jstatusOpt.setSelectedItem(elem.getStatus());
+					jstat.setModel(jstatusOpt);
+					
+					JPanel dueDate = new JPanel();
+					JPanel startDate = new JPanel();
+					JPanel finishDate = new JPanel();
+					
+					dueDate.setLayout(new FlowLayout());
+					startDate.setLayout(new FlowLayout());
+					finishDate.setLayout(new FlowLayout());
+					
+					//JTextField dueDateMonth = new JTextField(7);
+					JTextField dueDateDay = new JTextField(7);
+	
+					//declare start date combo box
+					jmonthstartdate = new JComboBox();
+					JTextField dateStartedDay = new JTextField(7);
+					jmonthstartdate.setModel(jmonthstart);
+					
+					//declare finish date combo box
+					jmonthfinishdate = new JComboBox();	
+					JTextField dateFinishedDay = new JTextField(7);
+					jmonthfinishdate.setModel(jmonthend);
+					
+					//add due date
+					rightPanel.add(dueDate);
+					dueDate.add(jmonthduedate);
+					dueDate.add(dueDateDay);
+					
+					//add status
+					rightPanel.add(priorityField);
+					rightPanel.add(jstat);
+					
+					//add start date
+					rightPanel.add(startDate);
+					startDate.add(jmonthstartdate);
+					startDate.add(dateStartedDay);
+					
+					//add finish date
+					rightPanel.add(finishDate);
+					finishDate.add(jmonthfinishdate);
+					finishDate.add(dateFinishedDay);
+					
+					
+					//top panel
+					JPanel topPanel = new JPanel();
+					topPanel.setLayout(new FlowLayout());
+					topPanel.add(addTitle);
+					
+					//overall panel
+					JPanel allPanel = new JPanel();
+					allPanel.setLayout(new BorderLayout());
+					allPanel.add(topPanel, BorderLayout.NORTH);
+					allPanel.add(leftPanel, BorderLayout.WEST);
+					allPanel.add(rightPanel, BorderLayout.CENTER);
+					
+					//pop up
+					int result = JOptionPane.showConfirmDialog(null, allPanel, 
+				               "ADD", JOptionPane.OK_OPTION);
+					
+					//add to list here
+					if (result == JOptionPane.OK_OPTION) {
+						String des = descriptionField.getText();
+						int priority = Integer.parseInt(priorityField.getText());
+						String date = jmonthduedate.getSelectedItem() + " " + dueDateDay.getText();
+						String status = (String) jstat.getSelectedItem();
+						String StartDate = jmonthstartdate.getSelectedItem() + " " + dateStartedDay.getText();
+						String FinishDate = jmonthfinishdate.getSelectedItem() + " " + dateFinishedDay.getText();
+						
+						TDElement element = new TDElement(des, priority, date, status, StartDate, FinishDate);
+						list.update(element, jlist.getSelectedIndex());
+						
+						listItems.clear();
+	
+						for (int index = 0; index < list.getListSize(); index++)
+						{
+							TDElement element1 = list.getElement(index);
+							String ele = "<html>Description: " + element1.getDescription() + "<br/>"
+									+ "Due Date: " + element1.getDueDate() + "<br/>" 
+									+ "Priority: "+ element1.getPriority() + "<br/>" 
+									+ "Status: "+ element1.getStatus() + "<br/>" 
+									+ "Date Started: " + element1.getDateStarted() +"<br/>" 
+									+ "Date Finished: " + element1.getDateFinished() +  "<br/>" 
+									+ "--------------------------------------------------------</html>"; 
+							 listItems.add(ele);
+						}
+						jlist.updateUI();
+					}
+				}
+				
 			}
+			
+			
 			//when delete button is click
 			if (clicked == deleteButton)
 			{
-				list.remove(jlist.getSelectedIndex());
-				if(!jlist.isSelectionEmpty())
+				if(!jlist.isSelectionEmpty()) {
+					list.remove(jlist.getSelectedIndex());
 					listItems.clear();
-//				//temp
-				for (int index = 0; index < list.getListSize(); index++)
-				{
-					TDElement element1 = list.getElement(index);
-					String ele = "<html>Description: " + element1.getDescription() + "<br/>"
-							+ "Due Date: " + element1.getDueDate() + "<br/>" 
-							+ "Priority: "+ element1.getPriority() + "<br/>" 
-							+ "Status: "+ element1.getStatus() + "<br/>" 
-							+ "Date Started: " + "<br/>" 
-							+ "Date Finished: " + "<br/>" 
-							+ "--------------------------------------------------------</html>"; 
-					 listItems.add(ele);
+					for (int index = 0; index < list.getListSize(); index++)
+					{
+						TDElement element1 = list.getElement(index);
+						String ele = "<html>Description: " + element1.getDescription() + "<br/>"
+								+ "Due Date: " + element1.getDueDate() + "<br/>" 
+								+ "Priority: "+ element1.getPriority() + "<br/>" 
+								+ "Status: "+ element1.getStatus() + "<br/>" 
+								+ "Date Started: " + "<br/>" 
+								+ "Date Finished: " + "<br/>" 
+								+ "--------------------------------------------------------</html>"; 
+						 listItems.add(ele);
+					}
+					jlist.updateUI();
+					jlist.setSelectedValue(null, false);
 				}
-				jlist.updateUI();
 			}
 		}
 	}
