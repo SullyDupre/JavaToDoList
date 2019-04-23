19fe74ae02b817be4e862c58d278bec9421ae445
tnguy107 <tnguy107@ToanDesktop>
2019-04-22 17:34:52 -0700

    add test working

--- cse360ToDoList/src/cse360ToDoList/TextPanel.java
+++ cse360ToDoList/src/cse360ToDoList/TextPanel.java
@@ -9,13 +9,15 @@ import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.event.*;
 import java.util.*;
-public class TextPanel extends JPanel implements ActionListener {
+public class TextPanel extends JPanel {
 	
+	TDList list = new TDList();
 	private JScrollPane scrollPane;
 	private JList jlist;
 	private JButton editButton;
 	private JButton deleteButton;
 	private JButton addButton;
+	private Vector listItems;
 	
 	/**
 	 * constructor: create an area that display all of the to do 
@@ -23,31 +25,21 @@ public class TextPanel extends JPanel implements ActionListener {
 	 */
 	public TextPanel() {
 		//create an array of string that contains all the info
-		String[] listItems = new String[5];
-		for (int index = 0; index < 5; index++)
-		{
-			listItems[index] = "<html>Description: " + "<br/>"
-					+ "Due Date: " + "<br/>" 
-					+ "Priority: " + "<br/>" 
-					+ "Status: " + "<br/>" 
-					+ "Date Started: " + "<br/>" 
-					+ "Date Finished: " + "<br/>" 
-					+ "--------------------------------------------------------</html>"; 
-		}
+
 		//set up layout
 		jlist = new JList(listItems);
 		jlist.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		jlist.addListSelectionListener(new ListListener());
+
 		JScrollPane scrollPane = new JScrollPane(jlist);
 		setLayout(new BorderLayout());
 		add(scrollPane, BorderLayout.CENTER);
 		
-		editButton = new JButton("EDIT      ");
+		editButton = new JButton("EDIT");
 		deleteButton = new JButton("DELETE");
 		addButton = new JButton ("SAVE");
-		editButton.addActionListener(this);
-		deleteButton.addActionListener(this);
-		addButton.addActionListener(this);
+		editButton.addActionListener(new ListListener());
+		deleteButton.addActionListener(new ListListener());
+		addButton.addActionListener(new ListListener());
 		JPanel panel = new JPanel();
 		panel.setLayout(new GridLayout(3, 1));
 		//panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
@@ -57,108 +49,117 @@ public class TextPanel extends JPanel implements ActionListener {
 		add(panel, BorderLayout.EAST);
 	}
 	
-	/** 
-	 * This method call the appropriate methods when a button is clicked
-	 * @param take in an clicked event
-	 */
-	public void actionPerformed(ActionEvent e) {
-		JButton clicked = (JButton)e.getSource();
-		
-		//when add button is click
-		if (clicked == addButton)
-		{
-			//labels
-			JLabel addTitle = new JLabel("Add");
-			addTitle.setFont(new Font("Time New Roman", Font.BOLD, 20));
-			JLabel descriptionLabel = new JLabel("Description");
-			JLabel dueDateLabel = new JLabel("Due Date");
-			JLabel priorityLabel = new JLabel("Priority");
-			JLabel statusLabel = new JLabel("Status");
-			JLabel dateStartedLabel = new JLabel("Date Started");
-			JLabel dateFinishedLabel = new JLabel("Date Finished");
-			
-			//text fields with 20 characters in each each
-			JTextField descriptionField = new JTextField(20);
-			JTextField dueDateField = new JTextField(20);
-			JTextField priorityField = new JTextField(20);
-			JTextField statusField = new JTextField(20);
-			JTextField dateStartedField = new JTextField(20);
-			JTextField dateFinishedField = new JTextField(20);
-			
-			//left panel
-			JPanel leftPanel = new JPanel();
-			leftPanel.setLayout(new GridLayout(6,1));
-			leftPanel.add(descriptionLabel);
-			leftPanel.add(dueDateLabel);
-			leftPanel.add(priorityLabel);
-			leftPanel.add(statusLabel);
-			leftPanel.add(dateStartedLabel);
-			leftPanel.add(dateFinishedLabel);
-			
-			//right panel
-			JPanel rightPanel = new JPanel();
-			rightPanel.setLayout(new GridLayout(6,1));
-			rightPanel.add(descriptionField);
-			rightPanel.add(dueDateField);
-			rightPanel.add(priorityField);
-			rightPanel.add(statusField);
-			rightPanel.add(dateStartedField);
-			rightPanel.add(dateFinishedField);
-			
-			//top panel
-			JPanel topPanel = new JPanel();
-			topPanel.setLayout(new FlowLayout());
-			topPanel.add(addTitle);
-			
-			//overall panel
-			JPanel allPanel = new JPanel();
-			allPanel.setLayout(new BorderLayout());
-			allPanel.add(topPanel, BorderLayout.NORTH);
-			allPanel.add(leftPanel, BorderLayout.WEST);
-			allPanel.add(rightPanel, BorderLayout.CENTER);
-			
-			//pop up
-			int result = JOptionPane.showConfirmDialog(null, allPanel, 
-		               "ADD", JOptionPane.OK_OPTION);
-			
-			//add to list here
-			if (result == JOptionPane.OK_OPTION) {
-				String des = descriptionField.getText();
-				int priority = Integer.parseInt(priorityField.getText());
-				String date = dueDateField.getText();
-				String status = statusField.getText();
-				TDElement element = new TDElement(des, priority, new Date(), 0);
-				
-			}
-			
-		}
-		//when edit button is click
-		if (clicked == editButton)
-		{
-		}
-		//when delete button is click
-		if (clicked == deleteButton)
-		{
-		}
-		
-	}
+
 	
 
 	/**
 	 * This method is used the index of JList to use in edit and delete method
 	 */
-	private class ListListener implements ListSelectionListener
+	private class ListListener implements ActionListener
 	{
-		public void valueChanged(ListSelectionEvent event)
-		{
-			//nothing selected
-			if (jlist.isSelectionEmpty())
-			{}
-			else //selected (for edit and delete)
-			{}
+		/** 
+		 * This method call the appropriate methods when a button is clicked
+		 * @param take in an clicked event
+		 */
+		public void actionPerformed(ActionEvent e) {
+			JButton clicked = (JButton)e.getSource();
 			
+			//when add button is click
+			if (clicked == addButton)
+			{
+				//labels
+				JLabel addTitle = new JLabel("Add");
+				addTitle.setFont(new Font("Time New Roman", Font.BOLD, 20));
+				JLabel descriptionLabel = new JLabel("Description");
+				JLabel dueDateLabel = new JLabel("Due Date");
+				JLabel priorityLabel = new JLabel("Priority");
+				JLabel statusLabel = new JLabel("Status");
+				JLabel dateStartedLabel = new JLabel("Date Started");
+				JLabel dateFinishedLabel = new JLabel("Date Finished");
+				
+				//text fields with 20 characters in each each
+				JTextField descriptionField = new JTextField(20);
+				JTextField dueDateField = new JTextField(20);
+				JTextField priorityField = new JTextField(20);
+				JTextField statusField = new JTextField(20);
+				JTextField dateStartedField = new JTextField(20);
+				JTextField dateFinishedField = new JTextField(20);
+				
+				//left panel
+				JPanel leftPanel = new JPanel();
+				leftPanel.setLayout(new GridLayout(6,1));
+				leftPanel.add(descriptionLabel);
+				leftPanel.add(dueDateLabel);
+				leftPanel.add(priorityLabel);
+				leftPanel.add(statusLabel);
+				leftPanel.add(dateStartedLabel);
+				leftPanel.add(dateFinishedLabel);
+				
+				//right panel
+				JPanel rightPanel = new JPanel();
+				rightPanel.setLayout(new GridLayout(6,1));
+				rightPanel.add(descriptionField);
+				rightPanel.add(dueDateField);
+				rightPanel.add(priorityField);
+				rightPanel.add(statusField);
+				rightPanel.add(dateStartedField);
+				rightPanel.add(dateFinishedField);
+				
+				//top panel
+				JPanel topPanel = new JPanel();
+				topPanel.setLayout(new FlowLayout());
+				topPanel.add(addTitle);
+				
+				//overall panel
+				JPanel allPanel = new JPanel();
+				allPanel.setLayout(new BorderLayout());
+				allPanel.add(topPanel, BorderLayout.NORTH);
+				allPanel.add(leftPanel, BorderLayout.WEST);
+				allPanel.add(rightPanel, BorderLayout.CENTER);
+				
+				//pop up
+				int result = JOptionPane.showConfirmDialog(null, allPanel, 
+			               "ADD", JOptionPane.OK_OPTION);
+				
+				//add to list here
+				if (result == JOptionPane.OK_OPTION) {
+					String des = descriptionField.getText();
+					int priority = Integer.parseInt(priorityField.getText());
+					String date = dueDateField.getText();
+					String status = statusField.getText();
+					TDElement element = new TDElement(des, priority, new Date(), 1);
+					list.insert(element);
+					
+//					//temp
+					for (int index = 0; index < list.getListSize(); index++)
+					{
+						TDElement element1 = list.getElement(index);
+						String ele = "<html>Description: " + element1.getDescription() + "<br/>"
+								+ "Due Date: " + element1.getDueDate() + "<br/>" 
+								+ "Priority: "+ element1.getPriority() + "<br/>" 
+								+ "Status: "+ element1.getStatus() + "<br/>" 
+								+ "Date Started: " + "<br/>" 
+								+ "Date Finished: " + "<br/>" 
+								+ "--------------------------------------------------------</html>"; 
+						 listItems.add(ele);
+					}
+					jlist.updateUI();
+				}
+				
+			}
+			//when edit button is click
+			if (clicked == editButton)
+			{
+			}
+			//when delete button is click
+			if (clicked == deleteButton)
+			{
+			}
 		}
 	}
+	
+	
+
 }
 
 
